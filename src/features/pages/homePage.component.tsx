f5a3526260b62a9511ce210da7a471a5b94f632e
3belpolin <3belpolin@gmail.com>
2025-09-19 12:35:35 +0300

    feat: homepage finished

--- src/features/pages/homePage.component.tsx
+++ src/features/pages/homePage.component.tsx
@@ -1,14 +1,25 @@
+import { Button } from '@ui/button/button'
 import styles from './homePage.module.scss'
 
 export const HomePage = () => {
   return (
     <section className={styles.homepage}>
-      <h1 className={styles.homepage__h1}>
-        The history of Vilnius 
-        <span className={styles.homepage__h1__span}>in events</span>
-      </h1>
 
-      <div className={styles.homepage__background}></div>
+      <div className={styles.homepage__wrapper}>
+        <div className={styles.homepage__wrapper__left}>
+        <h1 className={styles.homepage__h1}>
+        The history of Vilnius 
+        <span className={styles.homepage__h1__span}> in events</span>
+      </h1>
+      </div>
+
+      <div className={styles.homepage__wrapper__right}>
+        <Button size='lg' className={styles.homepage__button}>Explore <img src="src\assets\images\arrow_right_alt.svg" alt="" /></Button>
+
+      </div>
+
+      </div>
+ 
     </section>
   )
 }
